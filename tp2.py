49be2f1404d672d4f6bb1d8dea52fb2d60fab1ba
Jaime Montealegre <jaime.montealegre@jellysmack.com>
2024-02-10 22:18:17 +0100

    updating tests

--- tp2.py
+++ tp2.py
@@ -14,6 +14,11 @@ def my_segmentation(img, img_mask, seuil):
     return img_out
 
 def evaluate(img_out, img_GT):
+    '''
+        inputs: 
+            img_out: segmentation
+            img_GT: verite terrain
+    '''
     GT_skel  = thin(img_GT, max_num_iter = 15) # On suppose que la demie epaisseur maximum 
     img_out_skel  = thin(img_out, max_num_iter = 15) # d'un vaisseau est de 15 pixels...
     TP = np.sum(img_out_skel & img_GT) # Vrais positifs
@@ -22,40 +27,47 @@ def evaluate(img_out, img_GT):
 
     ACCU = TP / (TP + FP) # Precision
     RECALL = TP / (TP + FN) # Rappel
-    return ACCU, RECALL, img_out_skel, GT_skel
+    # return ACCU, RECALL, img_out_skel, GT_skel
+    print(f"Accuracy = {ACCU}, Recall = {RECALL}")
 
-#Ouvrir l'image originale en niveau de gris
-img =  np.asarray(Image.open('./images_IOSTAR/star01_OSC.jpg')).astype(np.uint8)
-print(img.shape)
+def main():
+    #Ouvrir l'image originale en niveau de gris
+    img =  np.asarray(Image.open('./images_IOSTAR/star01_OSC.jpg')).astype(np.uint8)
+    print(img.shape)
 
-nrows, ncols = img.shape
-row, col = np.ogrid[:nrows, :ncols]
-#On ne considere que les pixels dans le disque inscrit 
-img_mask = (np.ones(img.shape)).astype(np.bool_)
-invalid_pixels = ((row - nrows/2)**2 + (col - ncols/2)**2 > (nrows / 2)**2)
-img_mask[invalid_pixels] = 0
+    nrows, ncols = img.shape
+    row, col = np.ogrid[:nrows, :ncols]
+    #On ne considere que les pixels dans le disque inscrit 
+    img_mask = (np.ones(img.shape)).astype(np.bool_)
+    invalid_pixels = ((row - nrows/2)**2 + (col - ncols/2)**2 > (nrows / 2)**2)
+    img_mask[invalid_pixels] = 0
 
-img_out = my_segmentation(img,img_mask,80)
+    img_out = my_segmentation(img,img_mask,80)
 
-#Ouvrir l'image Verite Terrain en booleen
-img_GT =  np.asarray(Image.open('./images_IOSTAR/GT_01.png')).astype(np.bool_)
+    #Ouvrir l'image Verite Terrain en booleen
+    img_GT =  np.asarray(Image.open('./images_IOSTAR/GT_01.png')).astype(np.bool_)
 
-ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out, img_GT)
-print('Accuracy =', ACCU,', Recall =', RECALL)
+    # ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out, img_GT)
+    evaluate(img_out, img_GT)
+    # print('Accuracy =', ACCU,', Recall =', RECALL)
+    img_out_skel = thin(img_out, max_num_iter = 15)
+    GT_skel = thin(img_GT, max_num_iter = 15)
+    plt.subplot(231)
+    plt.imshow(img,cmap = 'gray')
+    plt.title('Image Originale')
+    plt.subplot(232)
+    plt.imshow(img_out)
+    plt.title('Segmentation')
+    plt.subplot(233)
+    plt.imshow(img_out_skel)
+    plt.title('Segmentation squelette')
+    plt.subplot(235)
+    plt.imshow(img_GT)
+    plt.title('Verite Terrain')
+    plt.subplot(236)
+    plt.imshow(GT_skel)
+    plt.title('Verite Terrain Squelette')
+    plt.show()
 
-plt.subplot(231)
-plt.imshow(img,cmap = 'gray')
-plt.title('Image Originale')
-plt.subplot(232)
-plt.imshow(img_out)
-plt.title('Segmentation')
-plt.subplot(233)
-plt.imshow(img_out_skel)
-plt.title('Segmentation squelette')
-plt.subplot(235)
-plt.imshow(img_GT)
-plt.title('Verite Terrain')
-plt.subplot(236)
-plt.imshow(GT_skel)
-plt.title('Verite Terrain Squelette')
-plt.show()
+if __name__ == "__main__":
+    main()
